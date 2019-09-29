ea54b9c5d8be5c6075050c47d0579a1e0da5aa4c
Toohk <thomasdufayet3@gmail.com>
2019-09-29 16:17:05 +0200

    login/signup

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -16,4 +16,24 @@ class IndexController extends AbstractController
             'controller_name' => 'IndexController',
         ]);
     }
+
+    /**
+     * @Route("/signup", name="signup")
+     */
+    public function signup()
+    {
+        return $this->render('index/signup.html.twig', [
+            'controller_name' => 'IndexController',
+        ]);
+    }
+
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login()
+    {
+        return $this->render('index/login.html.twig', [
+            'controller_name' => 'IndexController',
+        ]);
+    }
 }
